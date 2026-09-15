d5b03f6778a6560207d615b2a16aedcf5f7c15c7
codebydj <jayad6889@gmail.com>
2026-09-15 18:29:48 +0530

    bump version to 3.1.6

--- src/config/appVersion.ts
+++ src/config/appVersion.ts
@@ -1,10 +1,10 @@
 /**
  * Centralized Application Version & Metadata Configuration for Spendly
- * SPENDLY V3.1.5
+ * SPENDLY V3.1.6
  */
 
-export const APP_VERSION = '3.1.5';
-export const APP_VERSION_NAME = 'Spendly V3.1.5';
+export const APP_VERSION = '3.1.6';
+export const APP_VERSION_NAME = 'Spendly V3.1.6';
 export const APP_BUILD_DATE = '16 September 2026';
 export const APP_NAME = 'Spendly';
 export const APP_PACKAGE_ID = 'com.spendly.finance';
